90ac3da712ad41740721f619f210cb9b88497774
sali <sangnvnkl@gmail.com>
2026-08-22 18:06:28 +0700

    Expand primitive array predicate type and edge-case coverage

--- plugin/trino-elasticsearch/src/test/java/io/trino/plugin/elasticsearch/TestElasticsearchArrayPredicateTranslator.java
+++ plugin/trino-elasticsearch/src/test/java/io/trino/plugin/elasticsearch/TestElasticsearchArrayPredicateTranslator.java
@@ -13,8 +13,11 @@
  */
 package io.trino.plugin.elasticsearch;
 
+import io.trino.plugin.elasticsearch.client.IndexMetadata.DateTimeType;
 import io.trino.plugin.elasticsearch.client.IndexMetadata.PrimitiveType;
+import io.trino.plugin.elasticsearch.decoders.BooleanDecoder;
 import io.trino.plugin.elasticsearch.decoders.IntegerDecoder;
+import io.trino.plugin.elasticsearch.decoders.TimestampDecoder;
 import io.trino.plugin.elasticsearch.decoders.VarcharDecoder;
 import io.trino.plugin.elasticsearch.expression.ElasticsearchRemotePredicate;
 import io.trino.spi.block.Block;
@@ -33,6 +36,7 @@ import java.util.Optional;
 import static io.airlift.slice.Slices.utf8Slice;
 import static io.trino.spi.type.BooleanType.BOOLEAN;
 import static io.trino.spi.type.IntegerType.INTEGER;
+import static io.trino.spi.type.TimestampType.TIMESTAMP_MILLIS;
 import static io.trino.spi.type.VarcharType.VARCHAR;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -56,6 +60,46 @@ public class TestElasticsearchArrayPredicateTranslator
 
         assertThat(ElasticsearchArrayPredicateTranslator.translate(contains, Map.of("numbers", column)))
                 .contains(new ElasticsearchRemotePredicate.Term("Numbers", 42L));
+    }
+
+    @Test
+    public void testContainsBooleanArray()
+    {
+        ArrayType arrayType = new ArrayType(BOOLEAN);
+        ElasticsearchColumnHandle column = new ElasticsearchColumnHandle(
+                List.of("Flags"),
+                arrayType,
+                new PrimitiveType("boolean"),
+                new BooleanDecoder.Descriptor("Flags"),
+                false);
+
+        Call contains = new Call(
+                BOOLEAN,
+                new FunctionName("contains"),
+                List.of(new Variable("flags", arrayType), new Constant(true, BOOLEAN)));
+
+        assertThat(ElasticsearchArrayPredicateTranslator.translate(contains, Map.of("flags", column)))
+                .contains(new ElasticsearchRemotePredicate.Term("Flags", true));
+    }
+
+    @Test
+    public void testContainsTimestampArray()
+    {
+        ArrayType arrayType = new ArrayType(TIMESTAMP_MILLIS);
+        ElasticsearchColumnHandle column = new ElasticsearchColumnHandle(
+                List.of("Times"),
+                arrayType,
+                new DateTimeType(List.of("strict_date_optional_time", "epoch_millis")),
+                new TimestampDecoder.Descriptor("Times"),
+                false);
+
+        Call contains = new Call(
+                BOOLEAN,
+                new FunctionName("contains"),
+                List.of(new Variable("times", arrayType), new Constant(1_000_000L, TIMESTAMP_MILLIS)));
+
+        assertThat(ElasticsearchArrayPredicateTranslator.translate(contains, Map.of("times", column)))
+                .contains(new ElasticsearchRemotePredicate.Term("Times", "1970-01-01T00:00:01"));
     }
 
     @Test
@@ -113,6 +157,24 @@ public class TestElasticsearchArrayPredicateTranslator
 
         assertThat(ElasticsearchArrayPredicateTranslator.translate(arraysOverlap, Map.of("numbers", column)))
                 .contains(new ElasticsearchRemotePredicate.Terms("Numbers", List.of(1L, 2L, 3L)));
+    }
+
+    @Test
+    public void testArraysOverlapWithEmptyConstantArrayRemainsResidual()
+    {
+        ArrayType arrayType = new ArrayType(INTEGER);
+        ElasticsearchColumnHandle column = new ElasticsearchColumnHandle(
+                List.of("Numbers"),
+                arrayType,
+                new PrimitiveType("integer"),
+                new IntegerDecoder.Descriptor("Numbers"),
+                false);
+        Call arraysOverlap = new Call(
+                BOOLEAN,
+                new FunctionName("arrays_overlap"),
+                List.of(new Variable("numbers", arrayType), new Constant(integerBlock(), arrayType)));
+
+        assertThat(ElasticsearchArrayPredicateTranslator.translate(arraysOverlap, Map.of("numbers", column))).isEmpty();
     }
 
     @Test
